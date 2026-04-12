7d5afd73c7d7e0844baab13cb333a27507789d7a
Tim Felsky <tim.felsky@gmail.com>
2026-04-12 10:33:32 -0400

    Use shield logo for favicon and Apple touch icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,19 +33,13 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
       },
     ],
-    apple: '/apple-icon.png',
+    shortcut: '/favicon-32x32.png',
+    apple: '/apple-touch-icon.png',
   },
 }
 
